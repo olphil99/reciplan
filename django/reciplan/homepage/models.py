3fe23a3494d643e9b4b95d8cf04414ac7abb5dad
Amber Sahdev <asahdev2@illinois.edu>
2020-03-24 17:47:53 -0500

    added models/DB structure

--- django/reciplan/homepage/models.py
+++ django/reciplan/homepage/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+# Each class corresponds to a new table in the sqlite3 database
+class Users(models.Model):
+    userID = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    bio = models.CharField(max_length=250)
+    location = models.CharField(max_length=30)
+    pictureURL = models.URLField()
+
+class UserFavorites(models.Model):
+    userID = models.ForeignKey(Users, on_delete=models.CASCADE, primary_key=True)
+    recipeID = models.CharField(max_length=50)
+
+    # workaround because Django doesnt have support for composite keys https://shashankvivek.in/2014/11/11/make-primary-key-with-two-or-more-field-in-django/
+    class Meta:
+        unique_together = (('userID', 'recipeID'),)
+
+class OwnsRecipes(models.Model):
+    userID = models.ForeignKey(Users, on_delete=models.CASCADE, primary_key=True)
+    recipeID = models.CharField(max_length=50)
+
+    # workaround because Django doesnt have support for composite keys https://shashankvivek.in/2014/11/11/make-primary-key-with-two-or-more-field-in-django/
+    class Meta:
+        unique_together = (('userID', 'recipeID'),)
+
+class Carts(models.Model):
+    userID = models.OneToOneField(Users, on_delete=models.CASCADE, primary_key=True)
+    recipeIDs = models.CharField(max_length=1000) # Can't find a way to store a list of items so we can just store the list as text and then write a python helper function to parse it when we extract it
+    dateUpdated = models.DateTimeField()
+
+
+# References:
+# Difference between Django ForeignKey, ManyToMany, OneToOne:
+# https://stackoverflow.com/questions/25386119/whats-the-difference-between-a-onetoone-manytomany-and-a-foreignkey-field-in-d
