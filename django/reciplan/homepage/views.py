f129340774447f8a90b7883ba6721b4347a05d9f
Amber Sahdev <asahdev2@illinois.edu>
2020-03-26 02:01:31 -0500

    added some backend code for index, results, and recipe page

--- django/reciplan/homepage/views.py
+++ django/reciplan/homepage/views.py
@@ -1,22 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import utils
 
 def index(request):
-    # Has a log in button, search bar 
-    return render(request, 'homepage/homepage.html')
+    # Has a search bar, log-in button
+    if request.method == 'POST':
+        query = request.POST['query']
+        return HttpResponseRedirect('/results/?query={0}'.format(query))
+    return render(request, 'homepage/homepage.html', context)
 
 def results(request):
-    # Page that shows list of results
-    return HttpResponse("results View")
+    # Page that shows list of search results
+    query = request.GET.get('query').strip().lower()
+    searchResults = utils.find_recipes(query)
+    context = {
+        'query':query,
+        'searchResults':searchResults
+    }
+
+    if request.method == 'POST':
+        selectedRecipeID = request.POST['selectedRecipe']
+        return HttpResponseRedirect('/recipe/?recipeID={0}'.format(selectedRecipeID))
+
+    return render(request, 'homepage/results.html', context)
 
 def recipe(request):
-    # Page that shows the recipe select from results
-    return HttpResponse("recipe View")
+    # Page that shows the recipe selected from results
+    recipeID = request.GET.get('recipeID').strip().lower()
+    recipeTitle, recipeText, recipeAuthor = utils.get_recipe_data(recipeID)
+    context = {
+        'recipeTitle':recipeTitle,
+        'recipeText':recipeText,
+        'recipeAuthor':recipeAuthor
+    }
+
+    return render(request, 'homepage/recipe.html', context)
 
 def login(request):
-    # Page that you use to login
+    # Page used to login
     return HttpResponse("login View")
 
 def cart(request):
     # Cart of the logged in user
     return HttpResponse("cart View")
+
+def newRecipe(request):
+    # Page to add a new recipe for a logged-in user
+    return HttpResponse("newRecipe View")
+
+def profile(request):
+    # User's profile
+    return
