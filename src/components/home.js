298f7645bf5eaa0106def7f55fbb34bb955e99a7
Olivia Phillips <olphil99@gmail.com>
2020-03-27 14:40:54 -0500

    call get

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import '../css/home.css';
+import axios from 'axios';
+const API_URL = "http://localhost:8000/api/students/";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ret: {}
+    }
+  }
+
+  componentDidMount() {
+    axios.get(API_URL)
+    .then(res => {
+      this.setState({ ret: res.date });
+    })
+    .catch(err => console.log(err));
+  }
+
   render() {
     return(
       <Container>
-      hey
+        {JSON.parse(this.state.ret)}
       </Container>
     )
   }
