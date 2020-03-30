57c22de3078b40bb7f4c54d79bd7720765f05927
Amber Sahdev <asahdev2@illinois.edu>
2020-03-29 19:44:56 -0500

    backend done

--- django/reciplan/api/utils.py
+++ django/reciplan/api/utils.py
@@ -6,10 +6,38 @@ def find_recipes(query):
     # returns a list of recipes that match the query
     # runs a SQL query against our DB to find the closest matches
     # input: query is a lower-case string
-    return [{'recipeName':'pasta'}, {'recipeName':'rice'}] # Mocked the data for now 
+    return [{'recipeName':'pasta'}, {'recipeName':'rice'}] # Mocked the data for now
 
 def get_recipe_data(recipeID):
-    # returns recipeTitle, recipeText, recipeAuthor
+    # returns recipeAuthor, recipeTitle, recipeIngredients, recipeInstructions, recipePictureURL
     # input: recipeID
     # Write SQL/MongoDB query here to find the recipe data
-    return '', '', ''
+    return '', '', '', '', ''
+
+def add_recipe(recipeOwner, recipeTitle, recipeIngredients, recipeInstructions, recipePictureURL):
+    # Adds a new recipe to MongoDB
+    # return True if successful, false if unsuccessful
+    return True
+
+def get_cart_data(userID):
+    # Returns cartItems (list of recipeNames), cartDateUpdated for loggen-in user userID
+    return [], ''
+
+def delete_from_cart(recipeID):
+    # Remove given recipeID from Cart Table
+    # return True if successful, false if unsuccessful
+    return True
+
+def authenticate_user(username, password):
+    # check if username password combination exists
+    # return true or false accordingly
+    return False
+
+def add_user(username, password, name, bio, location, pictureURL):
+    # add a new user to User table
+    # return true if successful, false if unsuccessful
+    return False
+
+def get_user_metadata(loggedInUser):
+    # returns name, bio, location, pictureURL, listOfRecipeNames of the loggedInUser
+    return '', '', '', '', ''
--- django/reciplan/api/views.py
+++ django/reciplan/api/views.py
@@ -1,8 +1,5 @@
 import json
 
-from django.shortcuts import render
-from django.http import HttpResponse
-
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -36,6 +33,7 @@ def results(request):
     }
     context = json.dumps(context)
 
+    # What happens when you select a recipe
     if request.method == 'POST':
         selectedRecipeID = request.POST['selectedRecipe']
         # TODO: how to handle post requests with React
@@ -45,36 +43,109 @@ def results(request):
 @api_view(['GET', 'POST'])
 def recipe(request):
     # Page that shows the recipe selected from results
-    recipeID = request.GET.get('recipeID').strip().lower()
-    recipeTitle, recipeText, recipeAuthor = utils.get_recipe_data(recipeID)
+    recipeID = '' # request.GET.get('recipeID').strip().lower()
+    recipeAuthor, recipeTitle, recipeIngredients, recipeInstructions, recipePictureURL = utils.get_recipe_data(recipeID)
     context = {
+        'recipeAuthor':recipeAuthor,
         'recipeTitle':recipeTitle,
-        'recipeText':recipeText,
-        'recipeAuthor':recipeAuthor
+        'recipeIngredients':recipeIngredients,
+        'recipeInstructions':recipeInstructions,
+        'recipePictureURL':recipePictureURL
     }
 
-    return render(request, 'api/recipe.html', context)
+    return Response(context)
 
 @api_view(['GET', 'POST'])
 def login(request):
     # Page used to login
+    # TODO
+    if request.method == 'POST':
+        username = request.GET.get('username').strip().lower()
+        password = request.GET.get('password').strip() # TODO: hash this password
+
+        userAuthenticated = utils.authenticate_user(username, password)
+        if userAuthenticated:
+            status = 'HTTP_202_ACCEPTED'
+        else:
+            status = 'HTTP_403_FORBIDDEN'
+
+    context = json.dumps({})
+    return Response(context)
+
+@api_view(['GET', 'POST'])
+def newUserRegistration(request):
+    # Page used to create a new user
+    if request.method == 'POST':
+        username = request.GET.get('username').strip().lower()
+        password = request.GET.get('password').strip() # TODO: Hash this password
+        name = request.GET.get('name').strip.lower()
+        bio = request.GET.get('bio').strip.lower()
+        location = request.GET.get('location').strip.lower()
+        pictureURL = request.GET.get('pictureURL').strip.lower()
+
+        isCreated = utils.add_user(username, password, name, bio, location, pictureURL)
+        if isCreated:
+            status = 'HTTP_201_CREATED'
+        else:
+            status = 'HTTP_400_BAD_REQUEST'
+
+        return Response(context, status=status)
+
     context = json.dumps({})
     return Response(context)
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def cart(request):
     # Cart of the logged in user
-    context = json.dumps({})
-    return Response(context)
+    if request.method == 'GET': # retreive items of cart
+        loggedInUser = '' # TODO: logged in userID
+        cartItems, cartDateUpdated = utils.get_cart_data(loggedInUser)
+        context = {'cartItems':cartItems, 'cartDateUpdated':cartDateUpdated}
+        context = json.dumps(context)
+        return Response(context)
+    elif request.method == 'POST': # deleting an item from cart
+        recipeID = request.GET.get('recipeToRemove').strip().lower()
+
+        isDeleted = utils.delete_from_cart(recipeID)
+        if isDeleted:
+            status = 'HTTP_201_CREATED'
+        else:
+            status = 'HTTP_400_BAD_REQUEST'
+
+        context = json.dumps({})
+        return Response(context, status=status)
+
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def newRecipe(request):
     # Page to add a new recipe for a logged-in user
     context = json.dumps({})
+    if request.method == 'POST':
+        recipeOwner = '' # TODO: logged in userID
+        recipeTitle = request.GET.get('recipeTitle').strip().lower()
+        recipeIngredients = request.GET.get('recipeIngredients').strip().lower()
+        recipeInstructions = request.GET.get('recipeInstructions').strip().lower()
+        recipePictureURL = request.GET.get('recipePictureURL').strip().lower()
+
+        isCreated = utils.add_recipe(recipeOwner, recipeTitle, recipeIngredients, recipeInstructions, recipePictureURL)
+        if isCreated:
+            status = 'HTTP_201_CREATED'
+        else:
+            status = 'HTTP_400_BAD_REQUEST'
+
+        return Response(context, status=status)
+
     return Response(context)
 
 @api_view(['GET', 'POST'])
 def profile(request):
     # User's profile
     context = json.dumps({})
+    userAuthenticated = False # TODO: add some way to autheticate user
+    if userAuthenticated:
+        loggedInUser = '' # TODO: logged in userID
+        name, bio, location, pictureURL, listOfRecipeNames = get_user_metadata(loggedInUser)
+
+        context = json.dumps({'name':name, 'bio':bio, 'location':location, 'pictureURL':pictureURL, 'listOfRecipeNames':listOfRecipeNames})
+
     return Response(context)
