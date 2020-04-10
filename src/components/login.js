e5d3341bf97b502fd9f40f9022bd4dee4c2114f2
jtisaac <jtisaac@users.noreply.github.com>
2020-04-10 13:53:36 -0500

    New User page

--- src/components/login.js
+++ src/components/login.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 // import '../css/profile.css';
 import axios from 'axios';
-import { SERVICE_URL } from '../utils.js';
+import { SERVICE_URL, UserProfile } from '../utils.js';
 
 /**
  * This is the page that pops up when a user needs to log in.
@@ -16,14 +16,67 @@ class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      userid: '',
+      password: '',
+      bio: ''
     }
+    this.validateNewUser = this.validateNewUser.bind(this);
+    this.postUser = this.postUser.bind(this);
+  }
+
+  async postUser(user) {
+    try {
+      const response = await axios.post(`${SERVICE_URL}/results/`, user); // change to an updated url
+      const data = await response.json();
+      return data;
+    } catch(e) {
+      console.log('Encountered an error posting a new user.');
+      return {};
+    }
+  }
+
+  // build the json from the form values and validate them
+  async validateNewUser(event) {
+    event.preventDefault();
+    let form = event.target;
+    console.log(form['userid'].value);
+    // get the password
+    let userJson = {
+      creatorId: 'makingNewLogin',
+      userid: form['userid'].value,
+      password: form['password'].value,
+      bio: form['bio'].value
+    };
+    let response = this.postUser(userJson);
+    // this.props.history.push('/search')
   }
 
   render() {
     return(
-      <Container id="login-window">
-        log
+      <Container>
+        <Form onSubmit={this.validateNewUser}>
+          <FormGroup>
+            <Row>
+              <Col sm="9">
+                <Label for="userid">New Username</Label>
+                <Input name="userid" id="userid" placeholder="Enter new Username" style={{width: '98%'}} required />
+              </Col>
+            </Row>
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Password</Label>
+            <Input name="password" id="password" placeholder="Enter Password" style={{width: '98%'}} required />
+          </FormGroup>
+          <FormGroup>
+            <Label for="bio">Bio</Label>
+            <Input name="bio" id="bio" placeholder="Enter Bio" style={{width: '98%'}} required />
+          </FormGroup>
+          <Row>
+            <Col sm="12">
+              <Button style={{width:'98%'}}>Submit</Button>
+            </Col>
+          </Row>
+        </Form>
       </Container>
     )
   }
